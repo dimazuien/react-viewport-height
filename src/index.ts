8b60760dd5a6a1c2059a591a2640511d09ab0e0a
Dima Zuien <dimazuien@gmail.com>
2020-12-20 18:08:59 +0200

    feat: add ssr check; remove vh on unmount; return vh from hook; make multiple use possible

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,42 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 
-const useVH = (): void => {
-  const setVH = useCallback(() => {
-    const vh = window.innerHeight * 0.01;
+const VARIABLE_NAME = '--vh';
 
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }, []);
+const calculateVH = () =>
+  // window check for server-side rendering
+  typeof window !== 'undefined' ? window.innerHeight * 0.01 : 0;
 
+const isVHSet = () =>
+  getComputedStyle(document.documentElement).getPropertyValue(VARIABLE_NAME) !==
+  '';
+
+const setVH = () => {
+  document.documentElement.style.setProperty(
+    VARIABLE_NAME,
+    `${calculateVH()}px`,
+  );
+};
+
+const removeVH = () => {
+  document.documentElement.style.removeProperty(VARIABLE_NAME);
+};
+
+const useVH = (): number => {
   useEffect(() => {
+    if (isVHSet()) return () => undefined;
+
+    setVH();
+
     window.addEventListener('resize', setVH);
 
-    return (): void => {
+    return () => {
       window.removeEventListener('resize', setVH);
-    };
-  }, [setVH]);
 
-  useEffect(setVH, [setVH]);
+      removeVH();
+    };
+  }, []);
+
+  return calculateVH();
 };
 
 export default useVH;
